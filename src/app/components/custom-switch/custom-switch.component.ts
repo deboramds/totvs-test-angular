89a7c375265c4c6def50cf540811dfb86cc16ac3
deboramds <deborah.mart@hotmail.com>
2025-08-28 22:21:46 -0300

    feat: cria pasta e arquivos do componente custom-switch

---
+++ src/app/components/custom-switch/custom-switch.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-switch',
+  templateUrl: './custom-switch.component.html',
+  styleUrls: ['./custom-switch.component.css']
+})
+export class CustomSwitchComponent {
+  @Input() value = false;
+  @Input() disabled = false;
+  @Output() valueChange = new EventEmitter<boolean>();
+
+  toggle() {
+    if (!this.disabled) {
+      this.value = !this.value;
+      this.valueChange.emit(this.value);
+    }
+  }
+}
